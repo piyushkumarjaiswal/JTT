a90ebbdeeac01ad5f6c09d9e3ffe636dfc139daa
Piyush <piyushkumarjaiswal@gmail.com>
2020-06-28 19:50:40 +0530

    Unit Test Cases
    Added unit Test Cases for funtional and flow aspects.

--- Jet2TravelTask/Jet2TravelTaskTests/Jet2TravelTaskTests.swift
+++ Jet2TravelTask/Jet2TravelTaskTests/Jet2TravelTaskTests.swift
@@ -10,25 +10,59 @@ import XCTest
 @testable import Jet2TravelTask
 
 class Jet2TravelTaskTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+ 
+  var manager: NetworkManager!
+  
+  /*Initialization of instances*/
+  override func setUp() {
+    manager = NetworkManager()
+  }
+  /*Tear down methode invoked after the code executes*/
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    manager = nil
+  }
+  /*Verifying api call getting Valid data*/
+  func testApiResponse() {
+    let expectations = expectation(description: "Api Response")
+    let param = ["page":"1","limit":"10"]
+    manager.fetchPost(detail: param, completion: {(detail, _) in
+      XCTAssert(detail != nil, "Data is not receiving from server")
+      expectations.fulfill()
+    })
+    waitForExpectations(timeout: 30) { error in
+      if let error = error {
+        print("Error: \(error.localizedDescription)")
+      }
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+  /*This test case fetches all person records*/
+  func testFetchFeedsFromCoreData() {
+    let results = CoreDataStack.sharedInstance.fetchCodeDataResult()
+    //Assert return numbers of todo items
+    //Asserts that two optional values are equal.
+    XCTAssert(((results?.count ?? 0) > 0), "Unable to find records in Core Data")
+  }
+  /*Verifying a valid rootcontroller is initalized*/
+  func testRootInitialization() {
+    if #available(iOS 13, *) {
+      let window =  UIApplication.shared.windows.first { $0.isKeyWindow }
+      XCTAssert(window?.rootViewController != nil, "Window having the root controller")
+    } else {
+      let window =  UIApplication.shared.keyWindow
+      XCTAssert(window?.rootViewController != nil, "Window having the root controller")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+  /*Verifying that storage data of sample.json file is in correct formate*/
+  func testLocalDataWithValidFormate() {
+    if let url = Bundle.main.url(forResource: "sample", withExtension: "json") {
+      do {
+        let data = try Data(contentsOf: url)
+        let dataModel = try? JSONDecoder().decode([FeedResult].self, from: data)
+        XCTAssert(dataModel != nil, "Data is incompatible with model object")
+      } catch {
+        print("error:\(error)")
+      }
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  }
 }
